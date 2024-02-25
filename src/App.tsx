db5562f0a0aab66d45b99b72fa61e7587f87c7d1
Kumar210 <kumar.manickam@kprinfo.com>
2024-02-25 09:13:56 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex justify-center items-center h-screen">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click me
+        </button>
+      </div>
     </div>
   );
 }
